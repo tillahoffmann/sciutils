50b851127b98c0c1d00d6f667051f70d1daf6c77
Till Hoffmann <tillahoffmann@gmail.com>
2021-06-07 13:39:10 +0100

    Add multidimensional bincount. (#15)

--- sciutils/util.py
+++ sciutils/util.py
@@ -1,4 +1,5 @@
 from collections import abc
+import numpy as np
 
 
 def dict_list_transpose(xs):
@@ -28,3 +29,43 @@ def dict_list_transpose(xs):
             for key, value in x.items():
                 y.setdefault(key, []).append(value)
     return y
+
+
+def bincountnd(xs, weights=None, minshape=None):
+    """
+    Count number of occurrences of each value in an array of non-negative
+    integer tuples.
+
+    Parameters
+    ----------
+    xs : array_like, 2 dimensions, non-negative integers
+        Input array with shape `(p, n)`, where `p` is the number of
+        dimensions of the output array and `n` is the number of indices.
+    weights : array_like, 1 dimension, optional
+        Weight vector of length `n` corresponding to indices in `xs`.
+    minshape : tuple, optional
+        A minimum shape for the output array.
+
+    Returns
+    -------
+    out : ndarray
+        The result of binning the input indices.
+    """
+    # Evaluate the shape of the output array.
+    if minshape is None:
+        minshape = 0
+    shape = [np.max(x) + 1 for x in xs]
+    shape = np.maximum(shape, minshape)
+    minlength = np.prod(shape)
+    # Evaluate the compressed index (i.e. the indices in a ravelled array).
+    compressed = 0
+    a = minlength
+    for dim, x in zip(shape, xs):
+        a //= dim
+        compressed = compressed + a * x
+
+    # Evaluate the bincount.
+    if weights is not None:
+        weights = weights.ravel()
+    bincount = np.bincount(compressed.ravel(), weights, minlength)
+    return bincount.reshape(shape)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,7 @@
 from sciutils import util as suu
+import itertools as it
+import numpy as np
+import pytest
 
 
 def test_transpose_dict_roundtrip():
@@ -12,3 +15,22 @@ def test_transpose_dict_roundtrip():
         'b': [3, 7],
     }
     assert list_of_dicts == suu.dict_list_transpose(dict_of_lists)
+
+
+@pytest.mark.parametrize('weights', [True, False])
+def test_bincountnd(weights):
+    shape = (3, 5, 7, 11)
+    ndim = len(shape)
+    n = 100
+    xs = (np.random.uniform(0, 1, (n, ndim)) * shape).astype(int)
+    weights = np.random.normal(0, 1, n) if weights else None
+    counts = suu.bincountnd(xs.T, weights)
+
+    assert counts.shape == shape
+    for idx in it.product(*[range(dim) for dim in shape]):
+        fltr = np.all(xs == idx, axis=1)
+        if weights is None:
+            count = fltr.sum()
+        else:
+            count = weights[fltr].sum()
+        assert counts[idx] == count
